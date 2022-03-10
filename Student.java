465ae85a940f4ca8c7e10460fc23044cbc5da712
Tommaso <tommasovangelderen@gmail.com>
2022-03-10 16:40:41 +0100

    Setters en getters toegevoegd

--- Student.java
+++ Student.java
@@ -6,4 +6,27 @@ public class Student {
     private String naam;
     private int studentennummer;
 
+    public ArrayList<Examen> getGehaald() {
+        return gehaald;
+    }
+
+    public int getStudentennummer() {
+        return studentennummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setStudentennummer(int studentennummer) {
+        this.studentennummer = studentennummer;
+    }
+
+    public void setGehaald(ArrayList<Examen> gehaald) {
+        this.gehaald = gehaald;
+    }
 }
