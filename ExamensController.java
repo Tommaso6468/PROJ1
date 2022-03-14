a2429265b59bcbc276de5b978d687302cab8184c
Jochem <29899660+JochCool@users.noreply.github.com>
2022-03-14 12:36:33 +0100

    Make kiesExamen return the Examen

--- ExamensController.java
+++ ExamensController.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class ExamensController {
 
-    ArrayList<Examen> examens = new ArrayList<Examen>();
+    ArrayList<Examen> examens = new ArrayList<>();
 
     public ArrayList<Examen> getExamens() {
         return examens;
@@ -17,6 +17,9 @@ public class ExamensController {
         examens.add(new Examen(naam, vragen, minimum));
     }
 
+    /**
+     * Drukt een lijst met alle examens af.
+     */
     public void lijstExamens() {
         int aantal = this.getAantalExamens();
         switch (aantal) {
@@ -29,10 +32,15 @@ public class ExamensController {
         }
     }
 
-    public boolean kiesEnStartExamen(Scanner scanner) {
+    /**
+     * Laat de gebruiker een examen uit de lijst kiezen.
+     * @param scanner De scanner om de gebruikersinvoer uit te lezen.
+     * @return De gekozen examen.
+     */
+    public Examen kiesExamen(Scanner scanner) {
         lijstExamens();
         int aantal = this.getAantalExamens();
-        if (aantal == 0) return;
+        if (aantal == 0) return null;
 
         while (true) {
             System.out.println("Geef het nummer van het examen dat u wilt afnemen.");
@@ -41,7 +49,7 @@ public class ExamensController {
                 System.out.println("Er is geen examen met dat nummer; probeer opnieuw.");
             }
             else {
-                return examens.get(index).neemAf();
+                return examens.get(index);
             }
         }
     }
