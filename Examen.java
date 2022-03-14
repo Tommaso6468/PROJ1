b6b7d12d1b3619af753011e9cd640bdd592b1e72
Jochem <29899660+JochCool@users.noreply.github.com>
2022-03-14 13:34:43 +0100

    Implement neemAf

--- Examen.java
+++ Examen.java
@@ -20,8 +20,22 @@ public class Examen {
         return naam;
     }
 
+    /**
+     * Neemt het examen af; stelt alle vragen in het examen en checkt of het minimum aantal goed is gehaald.
+     * @return True als het minimum aantal goed is gehaald; anders false.
+     */
     public boolean neemAf() {
-        // TODO
-        return false;
+        int aantalGoed = 0;
+        for (int i = 0; i < vragen.length; i++) {
+            System.out.println();
+            System.out.printf("Vraag %d:%n", i+1);
+            boolean isGoed = vragen[i].stelVraag();
+            if (isGoed) {
+                aantalGoed++;
+            }
+        }
+        System.out.println();
+        System.out.printf("Het examen is voltooid. U heeft %d vragen goed.%n", aantalGoed);
+        return aantalGoed >= minimum;
     }
 }
